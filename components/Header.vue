13b74a0bfa0e1bdd1b5a9ec3738cb7f338056f76
onions <dhdzch@gmail.com>
2025-02-18 23:52:15 -0600

    💅 Scss

--- components/Header.vue
+++ components/Header.vue
@@ -3,11 +3,35 @@
     <nav>
       <ul>
         <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/contact">Contact</NuxtLink></li>
       </ul>
     </nav>
   </header>
 </template>
 
 <script setup></script>
+
+<style lang="scss" scoped>
+// SCSS styles
+$primary-color: #3498db;
+
+header {
+  background-color: $primary-color;
+  color: white;
+  padding: 1rem;
+}
+
+nav ul {
+  list-style-type: none;
+  display: flex;
+  gap: 1rem;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+</style>
