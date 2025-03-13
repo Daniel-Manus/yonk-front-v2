2331039ba0de60ddfbd9ef3b2b1540b93fcadcaf
onions <dhdzch@gmail.com>
2025-03-12 18:02:35 -0600

    🦶 Footer adjustments

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,24 +1,26 @@
 <template>
   <footer>
-    <div class="links left">
-      <div class="link">info@yonk.online</div>
-    </div>
-    <div class="links right">
-      <a
-        class="link"
-        href="https://www.instagram.com/yonk.online/"
-        target="_blank"
-        >Instagram</a
-      >
-      <a class="link" href="https://twitter.com/yonkonline" target="_blank"
-        >Twitter</a
-      >
-      <a
-        class="link"
-        href="https://www.youtube.com/channel/UCeVIE2HS9uqD5cS0FrOapzw"
-        target="_blank"
-        >Youtube</a
-      >
+    <div class="footer-inner">
+      <div class="links left">
+        <div class="link">info@yonk.online</div>
+      </div>
+      <div class="links right">
+        <a
+          class="link"
+          href="https://www.instagram.com/yonk.online/"
+          target="_blank"
+          >Instagram</a
+        >
+        <a class="link" href="https://twitter.com/yonkonline" target="_blank"
+          >Twitter</a
+        >
+        <a
+          class="link"
+          href="https://www.youtube.com/channel/UCeVIE2HS9uqD5cS0FrOapzw"
+          target="_blank"
+          >Youtube</a
+        >
+      </div>
     </div>
   </footer>
 </template>
@@ -27,44 +29,32 @@
 
 <style lang="scss" scoped>
 footer {
-  .links {
-    display: inline-block;
-    font-size: 18px;
-    // padding: 30px 0 30px;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  background: $color-bg;
+}
 
-    @media only screen and (max-width: 800px) {
-      font-size: 15px;
-    }
+.footer-inner {
+  max-width: $maxwidth;
+  margin: 0 auto;
+  position: relative;
+  padding: 0 $mp;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-    &.left {
-      .link {
-        margin-left: 0;
-        &:hover {
-          color: black;
-          cursor: auto;
-        }
-      }
-    }
+.links {
+  font-size: 18px;
+  padding: $mp 0;
 
-    &.right {
-      float: right;
-    }
+  @media only screen and (max-width: 800px) {
+    font-size: 15px;
   }
-
-  .link {
-    cursor: pointer;
-    transition: 0s;
-    margin-left: 24px;
-
-    &:hover {
-      color: $color-hl;
-    }
-
-    @media only screen and (max-width: 600px) {
-      display: block;
-      text-align: left;
-      width: 20vw;
-    }
-  }
+}
+.links.right {
+  display: flex;
+  gap: $mp / 2;
 }
 </style>
